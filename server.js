be64cf9e89411e4c090588b111b82400a146ea15
clghthd <clghthd@gmail.com>
2026-01-24 13:28:30 +0000

    chore: Prepare for Render deployment

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const db = new sqlite3.Database('database.db');
 
