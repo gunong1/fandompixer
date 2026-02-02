188022345746e6506063b3963bb90aa15f3b1043
gunong1 <clghthd@gmail.com>
2026-02-03 08:17:24 +0900

    fix(Pay): Restore explicit 'pg: html5_inicis' setting

--- main.js
+++ main.js
@@ -2162,7 +2162,7 @@ subscribeButton.onclick = async () => {
 
             response = await new Promise((resolve) => {
                 IMP.request_pay({
-                    // pg parameter removed to use Console Default
+                    pg: "html5_inicis",     // [FIX] Explicitly set to KG Inicis
                     pay_method: "card",
                     merchant_uid: paymentId,
                     name: `Idolpixel: ${pixelsToSend.length} pixels`,
